570cc98a17ecc1cfc0a6382be70403a923ec5309
Bob King <bob@someotherdude.com>
2017-08-27 19:38:03 -0400

    add random article link, add reset form feature

--- js/main.js
+++ js/main.js
@@ -6,19 +6,15 @@ function showResults(pages) {
     })
 }
 
-function showWikiPage(pageid) {
-
-}
-
 function writeResultsContainerToDOM(p) {
-    let parent = $("#mainContainer")
+    let parent = $(".results-container")
     let title = `<h4>${p.title}</h4>`
     let snippet = `<p>${p.extract}<p>`
     let elementID = `page-${p.pageid}`
     let myHTML = `<div class="row results" id="${elementID}"><div class="col-sm-12">${title}${snippet}</div></div>`
 
-    $("#mainContainer").append(myHTML)
-
+    $(parent).append(myHTML)
+    console.log("p: "+p.pageid);
     document.querySelector(`#page-${p.pageid}`).addEventListener('click', function(event) {
         goToWikipedia(p.pageid)
     });
@@ -41,7 +37,8 @@ function wikiSearch(q) {
             url: wikipediaAPI,
             dataType: 'json',
             success: function(data, status, jqXHR) {
-                //console.log(data.query.pages);
+                console.log(data.query.pages);
+                console.log("wikiSearch() successful");
                 showResults(data.query.pages)
             }
         })
@@ -56,32 +53,62 @@ function wikiSearch(q) {
         })
 }
 
-document.onkeypress = function (e) {
+document.onkeypress = function(e) {
     if (e.keyCode == 13) {
 
         $(".search-mover").animate({
             paddingTop: "30px"
-        },800)
-
-        window.setTimeout(function () {wikiSearch($("#my-search").val())},800)
+        }, 800)
+        $("#instr-top").text("Reset")
+        $("#instr-top").click(function functionName() {
+            resetForm()
+        })
+        window.setTimeout(function() {
+            wikiSearch($("#my-search").val())
+        }, 800)
     }
 }
 
-$("#my-search").click(function () {
+function resetForm() {
+    let rc = document.getElementById("results-container");
+
+    $("#my-search").val("")
+    rc.animate({
+        opactiy: 0
+    }, 400)
+
+    while (rc.hasChildNodes()) {
+        rc.removeChild(rc.lastChild);
+    }
+
+    $("#instr-top").text("Click circle to search Wikipedia")
+    window.setTimeout(function() {
+        $(".search-mover").animate({
+            paddingTop: "300px"
+        }, 800)
+    }, 400)
+    $("#my-search").animate({
+        width: "40px"
+    }, 400)
+}
+
+$("#my-search").click(function() {
     let f = $("#my-search")
     if (f.css("width") == "40px") {
         //do open
         f.animate({
-            width: "300px"}, 1000)
+            width: "300px"
+        }, 1000)
     } else {
         //do close
         f.animate({
-            width: "40px"}, 1000)
-            f.val("")
+            width: "40px"
+        }, 1000)
+        f.val("")
     }
 })
 
-$( window ).on( "load", function() {
+$(window).on("load", function() {
     $(".search-mover").animate({
         opacity: 1
     }, 400)
